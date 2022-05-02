2e86174fa1211c1f00eb71303202935bf6f50825
hppanpaliya <44992081+hppanpaliya@users.noreply.github.com>
2022-05-02 18:59:15 -0400

    Added Google Map on Search page

--- routes/search.js
+++ routes/search.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const router = express.Router();
-  
-  const {
-    searchDocs,
-  } = require("../models/doctors");
 
+const { searchDocs } = require("../models/doctors");
 
 router.get("/:specialty/:insurance", async (req, res) => {
-    let specialty = req.params.specialty;
-    let insurance = req.params.insurance;
-  
-    try {
-      const searchdocs = await searchDocs(specialty,insurance);
-      res.render('pages/search', {
-        title: "Search",
-        docs: searchdocs,
+  let specialty = req.params.specialty;
+  let insurance = req.params.insurance;
+
+  try {
+    const searchdocs = await searchDocs(specialty, insurance);
+    res.render("pages/search", {
+      title: "Search",
+      docs: searchdocs,
+      docsEncoded: encodeURIComponent(JSON.stringify(searchdocs)),
     });
-    } catch (e) {
-      res
-        .status(404)
-        .render("pages/error404", { error: e + "", title: "Error 404" });
-    }
-  });
-  module.exports = router;
-  
+  } catch (e) {
+    res
+      .status(404)
+      .render("pages/error404", { error: e + "", title: "Error 404" });
+  }
+});
+module.exports = router;
