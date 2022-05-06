e0b32e1104c6224c14eb17ea3cf213897ebbf177
Krithikagm <90419543+Krithikagm@users.noreply.github.com>
2022-05-06 16:36:33 -0400

    Update reviews.js

--- Seed_data/reviews.js
+++ Seed_data/reviews.js
@@ -4,34 +4,16 @@ const {ObjectId} = require('mongodb');
 const reviews = mongoCollections.reviews;
 const doctorList = mongoCollections.doctors;
 
-//Create a comment
+// Function to create reviews
 async function createReviews(doctorId,userID,review_text,rating){
-    if(!doctorId){
-        throw 'No Doctor ID was provided'
+    if(doctorId == null || userID == null || review_text == null || rating == null){
+        throw Error("INVALID: invalid review parameters")
     }
-    if(typeof doctorId!='string'){
-        throw 'Doctor ID must be a string'
+    if(typeof review_text !='string' || rating!='string'){
+        throw Error("Subject must be a string");
     }
-    if(doctorId.length==0){
-        throw 'Doctor ID cannot be empty'
-    }
-    if(!review_text){
-        throw 'No Subject was provided'
-    }
-    if(!rating){
-        throw 'No description was provided'
-    }
-    if(typeof review_text !='string'){
-        throw 'Subject must be a string'
-    }
-    if(typeof rating!='string'){
-        throw 'Description must be a string'
-    }
-    if(review_text.trim(' ').length===0){
-        throw 'Subject cannot contain only whitespaces'
-    }
-    if(rating.trim(' ').length===0){
-        throw 'Description cannot contain only whitespaces'
+    if(review_text.trim(' ').length===0 || rating.trim(' ').length===0){
+        throw Error("Subject cannot contain only whitespaces");
     }
     let x = ObjectId(doctorId);
     let rObj = {doctorId,userID,review_text,rating};
@@ -39,5 +21,4 @@ async function createReviews(doctorId,userID,review_text,rating){
     const insertComment = await getDoctor.updateOne({_id:x},{$push:{reviews:rObj}});
     return insertComment;
 }
-
 module.exports={createReviews}
